173f0c38738d130da65b53ffc997ea596087f23d
rohanfizz <rohans0501@gmail.com>
2022-05-05 01:39:20 +0530

    should work with backend in production

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         ENVIRONMENT: process.env.NODE_ENV === "production" ? "prod" : "dev",
         BACKEND_URL:
             process.env.NODE_ENV === "production"
-                ? ""
+                ? "https://mighty-coast-67371.herokuapp.com/"
                 : "http://localhost:8000/",
     },
 };
